1dbc69503f2ecef9880b08fe86bf79f9f9c38e52
Pantsu-It <479263213@qq.com>
2016-07-05 15:45:11 +0800

    commit package"activity"

---
+++ app/src/main/java/com/example/chen/freeparkingusers/activity/CampaignDetailActivity.java
@@ -0,0 +1,19 @@
+package com.example.chen.freeparkingusers.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.chen.freeparkingusers.R;
+
+/**
+ * Created by Pants on 2016/7/5.
+ */
+public class CampaignDetailActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compaign_detail);
+
+    }
+}
