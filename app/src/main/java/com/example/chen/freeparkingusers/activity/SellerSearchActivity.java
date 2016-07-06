c96a5573f4e1537ad7e1a6865fae9bdff9d2c0ef
Vector_victor <1113824260@qq.com>
2016-07-06 19:50:18 +0800

    添加一个Activity 和修改Manifests

---
+++ app/src/main/java/com/example/chen/freeparkingusers/activity/SellerSearchActivity.java
@@ -0,0 +1,12 @@
+package com.example.chen.freeparkingusers.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by chen on 16/7/6.
+ */
+public class SellerSearchActivity extends AppCompatActivity{
+
+
+
+}
