c88c8401fbdf8b50e5e91a177b08a65e953ef324
Vector_victor <1113824260@qq.com>
2016-07-06 15:33:31 +0800

    重新上传

--- app/src/main/java/com/example/chen/freeparkingusers/Fragments/SellerFragment.java
+++ app/src/main/java/com/example/chen/freeparkingusers/Fragments/SellerFragment.java
@@ -1,24 +1,56 @@
 package com.example.chen.freeparkingusers.Fragments;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.example.chen.freeparkingusers.R;
+import com.example.chen.freeparkingusers.adapter.SellerAdapter;
+import com.example.chen.freeparkingusers.item.SellerInfo;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by chen on 16/7/4.
  */
 public class SellerFragment extends BaseFragment{
 
+    private final String url = "";
+
+    private SwipeRefreshLayout mSwipeLayout;
+    private RecyclerView mRecyclerView;
+
+    private List<SellerInfo> mDatas;
+    private SellerAdapter mSellerAdapter;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container , Bundle SavedInstanceState){
         View view = inflater.inflate(R.layout.fragment_campaign,null,false);
-        tv = (TextView) view.findViewById(R.id.tv_campaign_test);
-        tv.setText(getTitle());
+        mSwipeLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipe_seller);
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.rv_seller);
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mDatas = new ArrayList<>();
+        getDatas();
+
+        mSellerAdapter = new SellerAdapter(getActivity(), (ArrayList<SellerInfo>) mDatas);
+        mRecyclerView.setAdapter(mSellerAdapter);
         return view;
     }
+
+    private void getDatas(){
+
+        for(int i = 0; i < 30 ; i++){
+            SellerInfo s = new SellerInfo(i+"","name"+i,"SellerImg","the place:"+i , "the contact:" +i);
+            mDatas.add(s);
+        }
+    }
+
 
 }
--- app/src/main/java/com/example/chen/freeparkingusers/activity/SellerDetailActivity.java
+++ app/src/main/java/com/example/chen/freeparkingusers/activity/SellerDetailActivity.java
@@ -3,8 +3,6 @@ package com.example.chen.freeparkingusers.activity;
 import android.app.Activity;
 import android.os.Bundle;
 
-import com.example.chen.freeparkingusers.R;
-
 /**
  * Created by Pants on 2016/7/5.
  */
@@ -13,7 +11,7 @@ public class SellerDetailActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_compaign_detail);
-
+//        setContentView(R.layout.activity_compaign_detail);
+//
     }
 }
