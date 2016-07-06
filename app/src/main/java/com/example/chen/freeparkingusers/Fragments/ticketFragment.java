03eebbb67a5bdebe15e2b8bf31957bb257d6790d
Vector_victor <1113824260@qq.com>
2016-07-06 15:28:17 +0800

    gogo

--- app/src/main/java/com/example/chen/freeparkingusers/Fragments/ticketFragment.java
+++ app/src/main/java/com/example/chen/freeparkingusers/Fragments/ticketFragment.java
@@ -4,20 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.example.chen.freeparkingusers.R;
 
 /**
  * Created by chen on 16/7/4.
  */
-public class TicketFragment extends BaseFragment{
+public class ticketFragment extends BaseFragment{
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container , Bundle SavedInstanceState){
         View view = inflater.inflate(R.layout.fragment_ticket,null,false);
-        tv = (TextView) view.findViewById(R.id.tv_ticket_test);
-        tv.setText(getTitle());
+//        tv = (TextView) view.findViewById(R.id.tv_ticket_test);
+//        tv.setText(getTitle());
         return view;
     }
 
