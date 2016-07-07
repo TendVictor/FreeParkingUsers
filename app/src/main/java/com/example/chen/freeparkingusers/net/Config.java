e6341a4a90159d73ec0bc1af4e92a8e6fdace522
Pantsu-It <479263213@qq.com>
2016-07-07 09:53:24 +0800

    commit from pantsu

--- app/src/main/java/com/example/chen/freeparkingusers/net/Config.java
+++ app/src/main/java/com/example/chen/freeparkingusers/net/Config.java
@@ -4,6 +4,7 @@ package com.example.chen.freeparkingusers.net;
  * Created by dell-pc on 2015/12/27.
  */
 public class Config {
+    public static final String URL_USER_GET_SELLER_INFO = "http://139.129.24.127/parking_app/User/user_searchActivityFromsellers.php";
 
 
     public static final String URL_SELLER_GET_SELLER_INFO = "http://139.129.24.127/parking_app/Seller/seller_getseller.php";
