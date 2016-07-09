28e025909ba854de02fd6f1d60c5a0d154237038
Pantsu-It <479263213@qq.com>
2016-07-09 22:09:12 +0800

    commit from pantsu

--- app/src/main/java/com/example/chen/freeparkingusers/view/ProgressImageView.java
+++ app/src/main/java/com/example/chen/freeparkingusers/view/ProgressImageView.java
@@ -1,5 +1,6 @@
-package com.example.chen.freeparkingusers.view;
+package com.example.pants.freeparkingtest.view;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
@@ -7,7 +8,9 @@ import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.RectF;
 import android.graphics.Xfermode;
+import android.os.Build;
 import android.util.AttributeSet;
+import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -32,7 +35,6 @@ public class ProgressImageView extends RelativeLayout {
         mShadowView = new ProgressShadowView(context);
         addView(mImageView, params1);
         addView(mShadowView, params1);
-
     }
 
     public ImageView getImageView() {
@@ -44,6 +46,7 @@ public class ProgressImageView extends RelativeLayout {
             throw new RuntimeException("Method setProgress: OutOfBoundsException");
 
         mShadowView.setProgress(progress);
+        invalidate();
     }
 
     public void setProgressEnable(boolean enable) {
@@ -54,22 +57,33 @@ public class ProgressImageView extends RelativeLayout {
 
         private TextView mTextView;
         private int mProgress;
-        private boolean mEnable;
+        private boolean mEnable = false;
 
-        private Paint paint;
-        private Xfermode mXfermode = new PorterDuffXfermode(PorterDuff.Mode.DST_OUT);
+        private Paint mPaint;
+        private RectF mRectF;
+        private Xfermode mXfermode;
 
         public ProgressShadowView(Context context) {
             super(context);
             setWillNotDraw(false);
-            paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+            mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+            mXfermode = new PorterDuffXfermode(PorterDuff.Mode.CLEAR);
 
             LayoutParams params2 = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
             params2.addRule(RelativeLayout.CENTER_IN_PARENT);
             mTextView = new TextView(context);
-            mTextView.setTextColor(0xff404040);
+            mTextView.setTextColor(0xff405040);
             mTextView.setTextSize(20);
             addView(mTextView, params2);
+
+            ViewTreeObserver observer = this.getViewTreeObserver();
+            observer.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+                @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+                @Override
+                public void onGlobalLayout() {
+                    mRectF = new RectF(0, 0, getWidth(), getHeight());
+                }
+            });
         }
 
         private void setProgress(int progress) {
@@ -93,21 +107,23 @@ public class ProgressImageView extends RelativeLayout {
         protected void onDraw(Canvas canvas) {
             if (!mEnable)
                 return;
-
             int progressY = (int) (getHeight() * (1 - mProgress / 100f));
 
+            // 建立图层
             int canvasWidth = canvas.getWidth();
             int canvasHeight = canvas.getHeight();
             int layerId = canvas.saveLayer(0, 0, canvasWidth, canvasHeight, null, Canvas.ALL_SAVE_FLAG);
-
-            RectF rectF = new RectF(0, 0, getWidth(), getHeight());
-            paint.setColor(0xeeeeeeee);
-            paint.setStyle(Paint.Style.FILL);
-            canvas.drawOval(rectF, paint);
-            paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
-            paint.setColor(0xffffffff);
-            canvas.drawRect(0, progressY, getWidth(), getHeight(), paint);
-            paint.setXfermode(null);
+            // 操作A
+            mPaint.setColor(0xaacccccc);
+            mPaint.setStyle(Paint.Style.FILL);
+            canvas.drawOval(mRectF, mPaint);
+            // 设置XferMode
+            mPaint.setXfermode(mXfermode);
+            // 操作B
+            canvas.drawRect(0, progressY, getWidth(), getHeight(), mPaint);
+            // 恢复paint
+            mPaint.setXfermode(null);
+            // 在栈中回复图层
             canvas.restoreToCount(layerId);
         }
     }
